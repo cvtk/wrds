76b2bd4f12172be7eb15a3ec33f3cd83f2f7182d
cvtk <olegcvetkov76@gmail.com>
2017-04-21 07:31:26 +0300

    add Vue.js

--- assets/a.js
+++ assets/a.js
@@ -1,16 +1,37 @@
 (function() {
   'use strict';
 
-  var app = {};
-  var mvc = {
-    Publications: Backbone.Firebase.Collection.extend({
-      url: 'https://wrds-5bb0a.firebaseio.com/publications'
-    })
-  };
+  var firebaseApp = firebase.initializeApp({
+      apiKey: 'AIzaSyBz-rXWMPsvuShKr9_O4uq0mazhrxbrow8',
+      authDomain: 'wrds-5bb0a.firebaseapp.com',
+      databaseURL: 'https://wrds-5bb0a.firebaseio.com',
+      projectId: 'wrds-5bb0a',
+      storageBucket: 'wrds-5bb0a.appspot.com',
+      messagingSenderId: '310931060091'
+    });
 
-  app.publications = new mvc.Publications();
-  app.publications.on('sync', function(data) {
-    console.log(data);
+  var app = {};
+  app.db = firebaseApp.database();
+  app.publications = new Vue({
+    el: '#publications',
+    firebase: {
+      anArray: app.db.ref('publications'),
+      anObject: {
+        source: app.db.ref('publications'),
+        asObject: true,
+        cancelCallback: function(error) {
+          console.log(error);
+        }
+      }
+    },
+    methods: {
+      reduceRating: function(key) {
+        app.publications.$firebaseRefs.anArray.child(key).child('rating').set('34');
+      },
+      increaseRating: function(key) {
+        app.publications.$firebaseRefs.anArray.child(key).child('rating').set('22');
+      },
+    }
   });
-  
+
 }());
